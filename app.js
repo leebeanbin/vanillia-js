e0914e60e468347c7dc9664bcecb14beadc63a16
leebeanbin <67886181+leebeanbin@users.noreply.github.com>
2020-08-19 12:51:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const ctx = canvas.getContext("2d");
 const  colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
 //pixel modifier에 size를 주지 않으면 코드를 위와 같이 짜도 실행이 되지 않는다.
 
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -82,12 +86,28 @@ function handelCanvasClick(event){
     
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+    //link.click() 이 클릭을 못하게 클릭한것을 거짓으로 만든다.
+}
+
 if(canvas){
     canvas.addEventListener("mousemove",mousedrag);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
     canvas.addEventListener("click", handelCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
+    // right click을 하면 나오는 메뉴를 contextmenu라고 하는데 이걸 preventDefault로 안나오게 막아 놓은 것임.
+    can
 }
 
 Array.from(colors).forEach(color => 
@@ -100,6 +120,10 @@ if(range){
 
 if(mode){
     mode.addEventListener("click",handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
 }
 
     //Array.from은 object로 부터 array를 만든다. forEach 함수는 주어진
